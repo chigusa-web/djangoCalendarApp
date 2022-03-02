6fc735f9aeb6cc5c971594b1e41d139e5c0e80ad
chigusa-web <chigusaweb66@gmail.com>
2022-03-03 00:12:06 +0900

    変更

--- djangoCalendarApp/settings.py
+++ djangoCalendarApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&98qd$dz!r0lg-^nospgcgu2y2uc&)hsaf8g$x4uebg@nct=z='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
