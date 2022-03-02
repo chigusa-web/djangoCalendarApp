dc29f7abe436472ac839a8494ba5f2db5bf181d2
chigusa-web <chigusaweb66@gmail.com>
2022-03-03 00:20:38 +0900

    イベントの登録

--- scheduleCalendar/static/scheduleCalendar/app.js
+++ scheduleCalendar/static/scheduleCalendar/app.js
@@ -3,7 +3,26 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+        initialView: 'dayGridMonth',
+
+        // 日付をクリック、または範囲を選択したイベント
+        selectable: true,
+        select: function (info) {
+            //alert("selected " + info.startStr + " to " + info.endStr);
+
+            // 入力ダイアログ
+            const eventName = prompt("イベントを入力してください");
+
+            if (eventName) {
+                // イベントの追加
+                calendar.addEvent({
+                    title: eventName,
+                    start: info.start,
+                    end: info.end,
+                    allDay: true,
+                });
+            }
+        },
     });
 
     calendar.render();
